3e76c553c168c25fb294ddf70c539affab8d7d51
Sacchidanand <sacchida222@gmail.com>
2019-04-19 19:33:18 +0530

    Added some tests

--- test/poly_test.dart
+++ test/poly_test.dart
@@ -59,14 +59,135 @@ void main() {
       [5, 6],
       [7, 8]
     ];
-    test("Old list Type:",(){
-      var oldType = correctDynamicList.runtimeType.toString();
-      expect(oldType,equals("List<dynamic>"));
-    });
-    test("Old list Length:",(){
-      var oldType = correctDynamicList.length;
-      expect(oldType,equals(4));
-    });
+    group("toListNum: ",(){
+      group("Old List: ",(){
+        test("Type",(){
+          var oldType = correctDynamicList.runtimeType.toString();
+          expect(oldType,equals("List<dynamic>"));
+        });
+        test("Length",(){
+          var oldLength = correctDynamicList.length;
+          expect(oldLength,equals(4));
+        });
+      });
+      group("without any optional parameters:",() {
+        List<num> lWithoutAnyOptional = toListNum(correctDynamicList);
+        test("Type", () {
+          var type = lWithoutAnyOptional.runtimeType.toString();
+          expect(type, equals("List<num>"));
+        });
+        test("Length", () {
+          var length = lWithoutAnyOptional.length;
+          expect(length, equals(2));
+        });
+        test("Value", () {
+          expect(lWithoutAnyOptional, equals([1, 2]));
+        });
+      });
+        group("with `replaceWithZero: true` and `sizeTwo: false`:",(){
+          List<num> lSizeNotTwo_Zero =
+          toListNum(correctDynamicList, replaceWithZero: true, sizeTwo: false);
+          test("Type",(){
+            var type = lSizeNotTwo_Zero.runtimeType.toString();
+            expect(type,equals("List<num>"));
+          });
+          test("Length",(){
+            var length = lSizeNotTwo_Zero.length;
+            expect(length,equals(4));
+          });
+          test("Value",(){
+            expect(lSizeNotTwo_Zero,equals([1,2,0,3]));
+          });
+        });
+      group("with `sizeTwo: false`:",(){
+        List<num> lSizeNotTwo = toListNum(correctDynamicList, sizeTwo: false);
+        test("Type",(){
+          var type = lSizeNotTwo.runtimeType.toString();
+          expect(type,equals("List<num>"));
+        });
+        test("Length",(){
+          var length = lSizeNotTwo.length;
+          expect(length,equals(3));
+        });
+        test("Value",(){
+          expect(lSizeNotTwo,equals([1,2,3]));
+        });
+      });
+    });//toListnum
 
-  });
+    group("toListListNum: ",(){
+        group("Old List: ",(){
+          test("Type",(){
+            var oldType = listOf.runtimeType.toString();
+            expect(oldType,equals("List<dynamic>"));
+          });
+          test("Length",(){
+            var oldLength = listOf.length;
+            expect(oldLength,equals(5));
+          });
+        });
+      group("without any optional parameters: ",() {
+        List<List<num>> m = toListListNum(listOf);
+        test("Type", () {
+          var type = m.runtimeType.toString();
+          expect(type, equals("List<List<num>>"));
+        });
+        test("Length", () {
+          var length = m.length;
+          expect(length, equals(3));
+        });
+        test("Value", () {
+          expect(m, equals([[3, 4], [5, 6], [7, 8]]));
+        });
+      });
+      group("with `swapXAndY: true`: ",(){
+        List<List<num>> mSwap = toListListNum(listOf, swapXAndY: true);
+        test("Type",(){
+          var type = mSwap.runtimeType.toString();
+          expect(type,equals("List<List<num>>"));
+        });
+        test("Length",(){
+          var length = mSwap.length;
+          expect(length,equals(3));
+        });
+        test("Value",(){
+          expect(mSwap,equals([[4, 3], [6, 5], [8, 7]]));
+        });
+      });
+      group("with `replaceWithZero: true`:",(){
+        List<List<num>> mZero = toListListNum(listOf, replaceWithZero: true);
+        test("Type",(){
+          var type = mZero.runtimeType.toString();
+          expect(type,equals("List<List<num>>"));
+        });
+        test("Length",(){
+          var length = mZero.length;
+          expect(length,equals(5));
+        });
+        test("Value",(){
+          expect(mZero,equals([[1, 0], [2, 0], [3, 4], [5, 6], [7, 8]]));
+        });
+      });
+        group("with `replaceWithZero: true` and `swapXAndY: true`: ",(){
+          List<List<num>> mZeroSwap =
+          toListListNum(listOf, replaceWithZero: true, swapXAndY: true);
+          test("Type",(){
+            var type = mZeroSwap.runtimeType.toString();
+            expect(type,equals("List<List<num>>"));
+          });
+          test("Length",(){
+            var length = mZeroSwap.length;
+            expect(length,equals(5));
+          });
+          test("Value",(){
+            expect(mZeroSwap,equals([[0, 1], [0, 2], [4, 3], [6, 5], [8, 7]]));
+          });
+        });
+    });//toListListnum
+  });//dynamic to num
+
+
+//  group("List => Point",(){
+//
+//  });
 }
